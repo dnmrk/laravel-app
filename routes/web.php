57df7cc02483d4e116d401199d0f26c0a6eddd37
Dean Mark Lamzon <dean@abovethefray.io>
2026-03-08 23:04:22 +0800

    Databases, Migrations, and Eloquent

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Idea;
 
 // Route::get('/', function () {
 //     return view('welcome', [
@@ -22,16 +24,31 @@ Route::get('/contact', function () {
 });
 
 Route::get('/', function () {
-    $ideas = session()->get('ideas', []);
+    // $ideas = session()->get('ideas', []);
+
+    // $ideas = DB::table('ideas')->get();
+    // $ideas = Idea::where('state', 'pending')->get();
+    // $idea = Idea::find(1);
+
+    $ideas = Idea::query()
+        ->when(request('state'), function ($query, $state) {
+            $query->where('state', $state);
+        })
+        ->get();
+
     return view('ideas', [
         'ideas' => $ideas,
     ]);
 });
 
 Route::post('/ideas', function () {
-    $idea = request('idea');
+    $idea = Idea::create([
+        'description' => request('idea'),
+        'state' => 'pending',
+    ]);
+
     // $idea = Request::input('idea');
-    session()->push('ideas', $idea);
+    // session()->push('ideas', $idea);
 
     return redirect('/');
 });
